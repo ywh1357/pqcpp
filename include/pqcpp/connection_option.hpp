86c87b8bab55115f51035b44ac6c552784a59dce
ywh1357 <ywh1357@gmail.com>
2019-07-01 13:30:59 +0800

    update

--- include/pqcpp/connection_option.hpp
+++ include/pqcpp/connection_option.hpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <memory>
 #include <fmt/format.h>
+#include <filesystem>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
 namespace pqcpp {
 
@@ -26,6 +29,21 @@ namespace pqcpp {
 		std::string get_conn_str() const {
 			return fmt::format("host={} dbname={} user={} password={}",
 				this->host, this->db, this->user, this->password);
+		}
+
+		static connection_options from_json(const std::filesystem::path& path) {
+			auto conf = nlohmann::json::parse(std::ifstream{ path });
+			return from_json(conf);
+		}
+
+		static connection_options from_json(const nlohmann::json& conf) {
+			connection_options opts;
+			opts.host = conf["host"];
+			opts.port = conf["port"];
+			opts.db = conf["db"];
+			opts.user = conf["user"];
+			opts.password = conf["password"];
+			return opts;
 		}
     };
 }
